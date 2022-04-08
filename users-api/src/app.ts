565402df583c64f9d0ab00a09f6cde16d287a59d
Marcus <marcus.vp@protonmail.com>
2022-04-08 12:21:34 -0300

    Initial users API solution commit.

---
+++ users-api/src/app.ts
@@ -0,0 +1,9 @@
+import express from 'express';
+
+const app = express();
+app.use(express.json());
+const port = 3000;
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}.`);
+});
