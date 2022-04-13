43db20bbd2146e69c6b6d1410845973e708629ee
Marcus <marcus.vp@protonmail.com>
2022-04-13 19:34:09 -0300

    Decoupling app and server.

--- users-api/src/app.ts
+++ users-api/src/app.ts
@@ -1,18 +1,11 @@
-import express from 'express';
-import dataSource from "./database/connection";
+import express from "express";
 import "utils/requestTimeout";
 
-import userRouter from "./routes/user";
+import userRouter from "routes/user";
 
 const app = express();
 app.use(express.json());
-const port = 3000;
 
-dataSource.initialize();
+app.use("/v1/users", userRouter);
 
-app.use("/api/users", userRouter);
-
-
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}.`);
-});
+export default app;
